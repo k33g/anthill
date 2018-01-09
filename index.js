6937faee7a579fbd2d00833192f9838b6f61c261
k33g <ph.charriere@gmail.com>
2018-01-09 22:57:59 +0100

    4

--- index.js
+++ index.js
@@ -13,7 +13,40 @@ let server = new mosca.Server(moscaSettings)
 
 function setup () {
   console.log('Mosca listening on ' + moscaSettings.port);
+  var connectionP = amqp.connect(process.env.RABBIT_URI);
+  connectionP.then(consumeRabbitEventQueue);
+  connectionP.catch(function(e) { console.error(e); });
+
 }
+
+function consumeRabbitEventQueue(connection) {
+  console.log("Connected to RabbitMQ.");
+  connection.createChannel().then(function(channel) {
+    var queue = process.env.RABBIT_QUEUE;
+    channel.assertQueue(queue, {durable: false}).then(function() {
+      channel.consume(queue, handleMessage, {noAck: true});
+    });
+  });
+};
+
+function handleMessage(message) {
+  var evt = JSON.parse(message.content);
+  var payload = evt.event;
+  if(payload != "CLEVER_TOOLS_REQUEST") {
+    sendMessage(payload);
+  }
+};
+
+function sendMessage(payload) {
+  server.publish({
+    topic: process.env.MQTT_TOPIC,
+    payload: payload,
+    qos: 0,
+    retain: false
+  }, function() {  });
+};
+
+
 
 server.on('clientConnected', function(client) {
   console.log('client connected', client.id);
