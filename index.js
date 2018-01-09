519af8b53393083ce7f8affda20eadf46c712db0
k33g <ph.charriere@gmail.com>
2018-01-09 22:50:11 +0100

    4

--- index.js
+++ index.js
@@ -10,80 +10,21 @@ let moscaSettings = {
 
 let server = new mosca.Server(moscaSettings)
 
-function sendMessage(payload) {
-  server.publish({
-    topic: process.env.MQTT_TOPIC,
-    payload: payload,
-    qos: 0,
-    retain: false
-  }, function() {  });
-};
-
-
-
-function handleMessage(message) {
-  var evt = JSON.parse(message.content);
-  var payload = evt.event;
-  if(payload != "CLEVER_TOOLS_REQUEST") {
-    sendMessage(payload);
-  }
-};
-
-
-function consumeRabbitEventQueue(connection) {
-  console.log("Connected to RabbitMQ.");
-  connection.createChannel().then(function(channel) {
-    var queue = process.env.RABBIT_QUEUE;
-    channel.assertQueue(queue, {durable: false}).then(function() {
-      channel.consume(queue, handleMessage, {noAck: true});
-    });
-  });
-};
-
 
 function setup () {
   console.log('Mosca listening on ' + moscaSettings.port);
-  server.authenticate = function(client, user, pwd, cb) {
-
-    console.log(client, user)
-
-    if(typeof user != 'null' && typeof pwd != 'null') {
-      if(user === process.env.AUTH_USER && pwd.toString() == process.env.AUTH_PASSWORD) {
-        client.user = user;
-        cb(null, true);
-      }
-    }
-  }
-  
-  var connectionP = amqp.connect(process.env.RABBIT_URI);
-  connectionP.then(consumeRabbitEventQueue);
-  connectionP.catch(function(e) { console.error(e); });
-
-  //generateDummyEvents();
-
 }
 
-server.on('ready', setup);
-server.on('clientConnected', function(client) { console.log('client connected: ' + client.id); });
-server.on('clientDisconnecting', function(client) { console.log('client disconnecting:' + client.id); });
-server.on('clientDisconnected', function(client) { console.log('client disconnected:' + client.id); });
-server.on('subscribed', function(topic, client) { console.log('client ' + client.id + ' subscribed to topic ' + topic); });
-server.on('unsubscribed', function(topic, client) { console.log('client ' + client.id + ' unsubscribed from topic ' + topic); });
-
-server.on('published', function(packet, client) {
-  var clientName;
-  if(client === null || typeof client == "undefined") {
-    clientName = 'an unknown client';
-  } else {
-    clientName = 'client ' + client.id;
-  }
-  console.log(
-    'Sent message to ' + clientName +
-    ' on topic ' + packet.topic + ': ' +
-    packet.payload + ' (messageId: ' + packet.messageId + ')'
-  );
+server.on('clientConnected', function(client) {
+  console.log('client connected', client.id);
 });
 
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
 
 
 // http part
