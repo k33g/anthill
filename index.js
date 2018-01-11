ca1191c141b6d5141e06064323b3f659a3cebfa0
k33g <ph.charriere@gmail.com>
2018-01-11 08:28:57 +0100

    experiment

--- index.js
+++ index.js
@@ -44,7 +44,12 @@ mqttBroker.on('unsubscribed', (topic, client) => {
 })
 mqttBroker.on('published', (packet, client) => {
 
+  console.log("⚠️", packet.cmd)
+  console.log("⚠️", packet)
+
   if(packet.cmd=="publish") {
+
+    console.log("❤️️️️️❤️❤️")
 
     let displayInformations = (client, packet, data) => {
 
